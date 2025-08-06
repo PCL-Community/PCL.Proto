a22cb71c469f6a442a6f3681eb3e6f39bee2e6e8
AMagicPear <AMagicPear@outlook.com>
2025-08-06 18:04:04 +0800

    fix: 优化侧边导航刷新按钮出现时机

--- src/components/widget/SideNavItem.vue
+++ src/components/widget/SideNavItem.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type { INavItem } from '@/types/naviOptions';
-import IconRefresh from '@/components/icons/side/IconRefresh.vue';
+import type { INavItem } from '@/types/naviOptions'
+import IconRefresh from '@/components/icons/side/IconRefresh.vue'
 defineProps<INavItem>()
 </script>
 
@@ -20,63 +20,63 @@ defineProps<INavItem>()
 
 <style scoped>
 .indicator {
-    visibility: hidden;
-    transform: scaleY(0);
-    transform-origin: center;
-    transition: none;
+  visibility: hidden;
+  transform: scaleY(0);
+  transform-origin: center;
+  transition: none;
 }
 
 a.router-link-active .indicator {
-    visibility: visible;
-    transform: scaleY(1);
-    transition: transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1);
+  visibility: visible;
+  transform: scaleY(1);
+  transition: transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1);
 }
 
 i.refresh-icon {
-    margin-left: auto;
-    color: var(--light-blue);
-    opacity: 0;
-    transition: 0.4s;
-    margin-right: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  margin-left: auto;
+  color: var(--light-blue);
+  opacity: 0;
+  transition: 0.4s;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 i.refresh-icon:hover {
-    color: var(--color-titlebar);
+  color: var(--color-titlebar);
 }
 
-a:hover i.refresh-icon {
-    opacity: 1;
+a.router-link-active:hover i.refresh-icon {
+  opacity: 1;
 }
 
 .side-nav-icon {
-    width: 20px;
-    height: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 a {
-    width: 100%;
-    display: flex;
-    gap: 11px;
-    align-items: center;
-    justify-content: flex-start;
-    font-size: 13px;
-    color: var(--color-text-black);
-    height: 34px;
-    background-color: transparent;
-    transition: 0.4s;
+  width: 100%;
+  display: flex;
+  gap: 11px;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 13px;
+  color: var(--color-text-black);
+  height: 34px;
+  background-color: transparent;
+  transition: 0.4s;
 }
 
 a:hover {
-    background-color: var(--color-tint-lighter);
+  background-color: var(--color-tint-lighter);
 }
 
 a.router-link-active {
-    color: var(--color-tint);
+  color: var(--color-tint);
 }
 </style>
