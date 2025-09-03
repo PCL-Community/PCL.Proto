5d015e03fb23af7a30687bbe23918b1800f656d4
AMagicPear <AMagicPear@outlook.com>
2025-09-04 00:37:54 +0800

    针对平台区分窗口设置

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,13 @@ pub fn run() {
                         .build(),
                 )?;
             }
+            #[cfg(target_os = "windows")]
+            {
+                // Windows 特供窗口设置
+                let main_window = app.get_webview_window("main").unwrap();
+                main_window.set_decorations(true)?;
+                main_window.set_title("PCL.Proto")?;
+            }
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![commands::launch_game])
