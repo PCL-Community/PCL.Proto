35757653777be792b02232bcc1b3df11e6d343cf
AMagicPear <AMagicPear@outlook.com>
2026-02-04 11:29:28 +0800

    refactor(scaffolding): 移除未使用的错误枚举并扩展EasyTier功能

--- src-tauri/src/scaffolding/easytier.rs
+++ src-tauri/src/scaffolding/easytier.rs
@@ -1,5 +1,11 @@
-use easytier::proto::common::NatType;
+use easytier::launcher::NetworkInstance;
+use easytier::proto::api::instance::ShowNodeInfoRequest;
+use easytier::proto::{
+    api::instance::ListRouteRequest, common::NatType, rpc_types::controller::BaseController,
+};
+use std::iter::once;
 use std::net::Ipv4Addr;
+use std::str::FromStr;
 
 /// 连接难度，根据EasyTier确定的网络结构转换而来
 pub enum ConnectionDifficulty {
@@ -13,7 +19,9 @@ pub enum ConnectionDifficulty {
 impl From<(&NatType, &NatType)> for ConnectionDifficulty {
     fn from(value: (&NatType, &NatType)) -> Self {
         let is = |types: &[NatType]| -> bool { types.contains(value.0) || types.contains(value.1) };
-        if is(&[NatType::OpenInternet]) {
+        if is(&[NatType::Unknown]) {
+            ConnectionDifficulty::Unknown
+        } else if is(&[NatType::OpenInternet]) {
             ConnectionDifficulty::Easiest
         } else if is(&[NatType::NoPat, NatType::FullCone]) {
             ConnectionDifficulty::Simple
@@ -31,4 +39,54 @@ pub struct EasyTierMember {
     pub address: Option<Ipv4Addr>,
     pub is_local: bool,
     pub nat: NatType,
+}
+
+trait EasyTierControl {
+    async fn get_members(&self) -> Option<Vec<EasyTierMember>>;
+
+    fn parse_address(address: Option<easytier::proto::common::Ipv4Inet>) -> Option<Ipv4Addr> {
+        address
+            .and_then(|address| address.address)
+            .map(|address| Ipv4Addr::from_octets(address.addr.to_be_bytes()))
+    }
+}
+
+impl EasyTierControl for NetworkInstance {
+    async fn get_members(&self) -> Option<Vec<EasyTierMember>> {
+        let api_service = self.get_api_service()?;
+        let service = api_service.get_peer_manage_service();
+        let (neighbours, this) = (
+            service
+                .list_route(BaseController::default(), ListRouteRequest::default())
+                .await
+                .ok()
+                .map(|response| response.routes)?,
+            service
+                .show_node_info(BaseController::default(), ShowNodeInfoRequest::default())
+                .await
+                .ok()
+                .map(|response| response.node_info)??,
+        );
+        Some(
+            neighbours
+                .into_iter()
+                .map(|route| EasyTierMember {
+                    hostname: route.hostname,
+                    address: Self::parse_address(route.ipv4_addr),
+                    nat: route
+                        .stun_info
+                        .map_or(NatType::Unknown, |info| info.udp_nat_type()),
+                    is_local: false,
+                })
+                .chain(once(EasyTierMember {
+                    hostname: this.hostname,
+                    address: Ipv4Addr::from_str(&this.ipv4_addr).ok(),
+                    is_local: true,
+                    nat: this
+                        .stun_info
+                        .map_or(NatType::Unknown, |info| info.udp_nat_type()),
+                }))
+                .collect::<Vec<_>>(),
+        )
+    }
 }
--- src-tauri/src/scaffolding/mod.rs
+++ src-tauri/src/scaffolding/mod.rs
@@ -5,28 +5,28 @@ pub mod mc;
 mod protocol;
 pub mod terracotta;
 
-/// 联机错误
-pub enum ScaffoldingError {
-    /// 房间码无效
-    InvalidRoomCode,
-    /// 联机中心未找到
-    HostNotFound,
-    /// 协议错误
-    ProtocolError(String),
-    /// 网络错误
-    NetworkError(String),
-    /// 其他错误
-    Other(String),
-}
+// /// 联机错误
+// pub enum ScaffoldingError {
+//     /// 房间码无效
+//     InvalidRoomCode,
+//     /// 联机中心未找到
+//     HostNotFound,
+//     /// 协议错误
+//     ProtocolError(String),
+//     /// 网络错误
+//     NetworkError(String),
+//     /// 其他错误
+//     Other(String),
+// }
 
-impl From<ScaffoldingError> for String {
-    fn from(err: ScaffoldingError) -> Self {
-        match err {
-            ScaffoldingError::InvalidRoomCode => "Invalid room code".to_string(),
-            ScaffoldingError::HostNotFound => "Host not found".to_string(),
-            ScaffoldingError::ProtocolError(msg) => format!("Protocol error: {}", msg),
-            ScaffoldingError::NetworkError(msg) => format!("Network error: {}", msg),
-            ScaffoldingError::Other(msg) => msg,
-        }
-    }
-}
+// impl From<ScaffoldingError> for String {
+//     fn from(err: ScaffoldingError) -> Self {
+//         match err {
+//             ScaffoldingError::InvalidRoomCode => "Invalid room code".to_string(),
+//             ScaffoldingError::HostNotFound => "Host not found".to_string(),
+//             ScaffoldingError::ProtocolError(msg) => format!("Protocol error: {}", msg),
+//             ScaffoldingError::NetworkError(msg) => format!("Network error: {}", msg),
+//             ScaffoldingError::Other(msg) => msg,
+//         }
+//     }
+// }
